07432a25ded0018e40b7cab4f12d61dabb589c5c
NikhileshKshirsagar <kshirsagar.nikhilesh@gmail.com>
2014-10-06 22:36:55 +0530

    User activities using REST connector
    1. Add headers to REST API call.
    2. Create model activities.
    3. Invoke REST call to get user activities.

--- server/boot/userActivities.js
+++ server/boot/userActivities.js
@@ -1,4 +1,24 @@
 var app = require('../server');
-
+var loopback = require('loopback');
 var ds = app.dataSources.fitbit;
+var activityModel = ds.createModel('Activities', {}, {base:loopback.Model});
 
+module.exports=activityModel;
+
+//activityModel.getUserActivities = function(data, cb){
+//	console.log(data);
+//	cb(null, data);
+//};
+
+//activityModel.getUserActivities.shared = true;
+//activityModel.getUserActivities.accepts = [{arg: 'date', type: 'string', http: {source: 'path'}}];
+//activityModel.getUserActivities.returns = [{arg: 'data', type: 'object', root: true} ];
+//activityModel.getUserActivities.http = {verb: 'get', path: '/:date'};
+
+// TODO : pass access token and timestamp
+activityModel.getUserActivities('2012-12-13', function(err, data){
+	console.log('Data: ', data);
+	console.log('Err: ', err);
+});
+
+app.model(activityModel);
