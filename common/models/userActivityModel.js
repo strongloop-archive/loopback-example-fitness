226fae5eb19b3a69c8d7e555efd05feda340784e
NikhileshKshirsagar <kshirsagar.nikhilesh@gmail.com>
2014-11-13 09:25:44 +0530

    Code formatting

--- common/models/userActivityModel.js
+++ common/models/userActivityModel.js
@@ -1,19 +1,27 @@
 var router = require('../../server/routes/index.js');
 
-module.exports = function(userActivityModel){
-	userActivityModel.getActivities = function(date, accessToken, accessSecret, cb) {
-		router.getActivities(date, accessToken, accessSecret, function(data){
-			cb(null, data);
-		});
+module.exports = function(userActivityModel) {
+  userActivityModel.getActivities = function(date, accessToken, accessSecret,
+      cb) {
+    router.getActivities(date, accessToken, accessSecret, function(data) {
+      cb(null, data);
+    });
   };
-   
-  userActivityModel.remoteMethod(
-      'getActivities',
-      {
-        accepts: [{arg: 'date', type: 'string'}, 
-                  {arg: 'accessToken', type: 'string'},
-                  {arg: 'accessSecret', type: 'string'}],
-        returns: {arg: 'greeting', type: 'json'}
-      }
-  );
+
+  userActivityModel.remoteMethod('getActivities', {
+    accepts : [ {
+      arg : 'date',
+      type : 'string'
+    }, {
+      arg : 'accessToken',
+      type : 'string'
+    }, {
+      arg : 'accessSecret',
+      type : 'string'
+    } ],
+    returns : {
+      arg : 'greeting',
+      type : 'json'
+    }
+  });
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -25,11 +25,11 @@ var acvtTemp = require('../views/actv.hbs');
 router.get('/auth/fitbit', passport.authenticate('fitbit'));
 
 router.get('/', function(req, res) {
-	res.redirect('/login');
+  res.redirect('/login');
 });
 
 router.get('/login', function(req, res) {
-	res.redirect('/auth/fitbit');
+  res.redirect('/auth/fitbit');
 });
 
 router.get('/auth/fitbit/callback', passport.authenticate('fitbit', {
@@ -39,82 +39,81 @@ router.get('/auth/fitbit/callback', passport.authenticate('fitbit', {
 });
 
 router.get('/auth/success', function(req, res) {
-	if (server.get('responseType') == "server") {
-		res.send(homeTemp({
-			'success' : 'Logged in successfully'
-		}));
-	}
+  if (server.get('responseType') == "server") {
+    res.send(homeTemp({
+      'success' : 'Logged in successfully'
+    }));
+  }
 
-	else if (server.get('responseType') == "app") {
-		res.send({
-			'status' : 'success'
-		});
-	}
+  else if (server.get('responseType') == "app") {
+    res.send({
+      'status' : 'success'
+    });
+  }
 });
 
 router.get('/auth/failure', function(req, res) {
-	/* User interface for serer side */
-	// res.send(homeTemp({
-	// 'failure' : 'Login unsuccessfull'
-	// }));
-	res.send({
-		'status' : 'failure'
-	});
+  /* User interface for serer side */
+  // res.send(homeTemp({
+  // 'failure' : 'Login unsuccessfull'
+  // }));
+  res.send({
+    'status' : 'failure'
+  });
 });
 
 router.get('/user/activities', function(req, res) {
-	console.log('Get activities');
-	var date = req.query.date;
-	// Set default date to get existing data from fitbit api
-	if (date === null || date == null)
-		date = '2012-12-10';
+  console.log('Get activities');
+  var date = req.query.date;
+  // Set default date to get existing data from fitbit api
+  if (date === null || date == null)
+    date = '2012-12-10';
 
-	date = moment(date).format('YYYY-MM-DD');
+  date = moment(date).format('YYYY-MM-DD');
 
-	userActivities.getUserActivities(server.get('clientID'), date, moment()
-	    .unix(), server.access.token, userActivities.generateSignature(date,
-	    server.access.token, server.access.secret, randomString), randomString,
-	    function(err, data) {
-		    data['date'] = moment(date).format('MM/DD/YYYY');
-		    debug('data: ' + JSON.stringify(data));
+  userActivities.getUserActivities(server.get('clientID'), date, moment()
+      .unix(), server.access.token, userActivities.generateSignature(date,
+      server.access.token, server.access.secret, randomString), randomString,
+      function(err, data) {
+        data['date'] = moment(date).format('MM/DD/YYYY');
+        debug('data: ' + JSON.stringify(data));
 
-		    returndata = data;
+        returndata = data;
 
-		    userProfile.getAttribute('fullName', server.access.userID, function(
-		        data) {
-			    if (returndata.summary) {
-				    returndata.summary['fullName'] = data;
+        userProfile.getAttribute('fullName', server.access.userID, function(
+            data) {
+          if (returndata.summary) {
+            returndata.summary['fullName'] = data;
 
-				    if (server.get('responseType') == "server") {
-					    res.send(activitiesTemp({
-						    'data' : returndata
-					    }));
-				    } else if (server.get('responseType') == "app") {
-					    res.send({
-						    'data' : returndata
-					    });
-				    }
-			    }
-			    else {
-				    res.send({
-					    'data' : returndata
-				    });
-			    }
-		    });
-	    });
+            if (server.get('responseType') == "server") {
+              res.send(activitiesTemp({
+                'data' : returndata
+              }));
+            } else if (server.get('responseType') == "app") {
+              res.send({
+                'data' : returndata
+              });
+            }
+          } else {
+            res.send({
+              'data' : returndata
+            });
+          }
+        });
+      });
 });
 
 router.getActivities = function(date, accessToken, accessSecret, callback) {
-	var formattedDate = moment(date).format('YYYY-MM-DD');
+  var formattedDate = moment(date).format('YYYY-MM-DD');
 
-	userActivities.getUserActivities(server.get('clientID'), formattedDate,
-	    moment().unix(), accessToken, userActivities.generateSignature(
-	        formattedDate, accessToken, accessSecret, randomString),
-	    randomString, function(err, data) {
-		    data['date'] = date;
-		    debug('data: ' + JSON.stringify(data));
-		    callback(data);
-	    });
+  userActivities.getUserActivities(server.get('clientID'), formattedDate,
+      moment().unix(), accessToken, userActivities.generateSignature(
+          formattedDate, accessToken, accessSecret, randomString),
+      randomString, function(err, data) {
+        data['date'] = date;
+        debug('data: ' + JSON.stringify(data));
+        callback(data);
+      });
 };
 
 // userActivities.remoteMethod(
@@ -124,5 +123,5 @@ router.getActivities = function(date, accessToken, accessSecret, callback) {
 // {arg: 'accessSecret', type: 'string', required: true},
 // {arg: 'date', type: 'date', required: true}],
 // returns: {arg: 'getacitivties', type: 'json'}
-//    }
-//);
+// }
+// );
